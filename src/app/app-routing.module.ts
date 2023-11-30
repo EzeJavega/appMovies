09867fdcd63245689c136c07c5fa16045fc23466
EzeJavega <ezejavega@hotmail.com>
2023-11-30 16:26:43 -0300

    Cambios iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppLayoutComponent,
+    children: [
+      {path:'', component: LandingComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
